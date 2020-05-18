5fe9f9bdaa6b28bddb7dc97b46e4612d5f520be9
Steve Brownlee <chortlehoort@gmail.com>
2020-05-18 14:40:36 -0500

    Using serializers to show line items as well

--- bangazonapi/views/order.py
+++ bangazonapi/views/order.py
@@ -10,8 +10,24 @@ from bangazonapi.models import Order, Payment, Customer, Product, OrderProduct
 from .product import ProductSerializer
 
 
+class OrderLineItemSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for line items """
+
+    product = ProductSerializer(many=False)
+
+    class Meta:
+        model = OrderProduct
+        url = serializers.HyperlinkedIdentityField(
+            view_name='lineitem',
+            lookup_field='id'
+        )
+        fields = ('id', 'product')
+        depth = 1
+
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for customer orders"""
+
+    lineitems = OrderLineItemSerializer(many=True)
 
     class Meta:
         model = Order
@@ -19,7 +35,7 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
             view_name='order',
             lookup_field='id'
         )
-        fields = ('id', 'url', 'created_date', 'payment_type', "customer")
+        fields = ('id', 'url', 'created_date', 'payment_type', 'customer', 'lineitems')
 
 
 class Orders(ViewSet):
